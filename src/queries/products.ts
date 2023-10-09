1446f8b236b9b43b2db8ce869b8c0a5abf14c049
taras957 <kornyaktaras@gmail.com>
2023-10-10 00:00:28 +0300

    feat: retrieve data from lambda

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -8,10 +8,10 @@ export function useAvailableProducts() {
   return useQuery<AvailableProduct[], AxiosError>(
     "available-products",
     async () => {
-      const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
+      const res = await axios.get<{ products: AvailableProduct[] }>(
+        `${API_PATHS.bff}/products`
       );
-      return res.data;
+      return res.data.products;
     }
   );
 }
