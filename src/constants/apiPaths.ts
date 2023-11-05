502825ebe2ea0c5b0453e2a874608a0f9a75c5e0
taras957 <kornyaktaras@gmail.com>
2023-11-05 23:58:24 +0200

    fix: auth-token

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://l1qz68pyb0.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://fa8jgd2trj.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
